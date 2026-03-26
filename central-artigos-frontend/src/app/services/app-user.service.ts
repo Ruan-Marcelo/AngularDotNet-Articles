b4b3503f4639cb9c30b89b07f7b47e03832e24c6
Ruan-Marcelo <luzruan60@gmail.com>
2026-03-26 16:49:27 -0300

    criação services root, rota httpClient/login + add theme

--- central-artigos-frontend/src/app/services/app-user.service.ts
+++ central-artigos-frontend/src/app/services/app-user.service.ts
@@ -2,12 +2,23 @@
  * Copyright (c) 2026 ruan Marcelo Ramacioti Luz.
  * Todos os direitos reservados.
  */
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppUserService {
+  url = environment.apiUrl;
 
-  constructor() { }
+  constructor(private httpClietn:HttpClient) { }
+
+  login(data:any){
+    return this.httpClietn.post(this.url+
+      "/appUser/login",data,{
+        headers: new HttpHeaders().set('Content-Type',"application/json")
+      }
+    )
+  }
 }
