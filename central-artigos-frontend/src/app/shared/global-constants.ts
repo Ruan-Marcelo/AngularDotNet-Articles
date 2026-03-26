72eeb0e848dd068e57269a4724a36f70cc5edf9a
Ruan-Marcelo <luzruan60@gmail.com>
2026-03-26 16:42:59 -0300

    tratamento de erro e mensagem em email

---
+++ central-artigos-frontend/src/app/shared/global-constants.ts
@@ -0,0 +1,12 @@
+export class Globalconstants{
+/*
+ * Copyright (c) 2026 ruan Marcelo Ramacioti Luz.
+ * Todos os direitos reservados.
+ */
+
+  //mensagem
+  public static genericError:string = "Algo deu errado. Por favor, tente novamente mais tarde."
+
+  //regex
+  public static emailRegex:string = "[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}";
+}
