c315ac427490f52dfeea4bf275709c5c944732f7
Ruan-Marcelo <luzruan60@gmail.com>
2026-03-26 16:58:10 -0300

    Troca o tema da aplicação Aplica classe no <body> Salva o tema no navegador Recupera o tema ao recarregar

--- central-artigos-frontend/src/app/services/theme.service.ts
+++ central-artigos-frontend/src/app/services/theme.service.ts
@@ -1,4 +1,9 @@
+/*
+ * Copyright (c) 2026 ruan Marcelo Ramacioti Luz.
+ * Todos os direitos reservados.
+ */
 import { Injectable } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +11,25 @@ import { Injectable } from '@angular/core';
 export class ThemeService {
 
   constructor() { }
+
+  settheme(theme:string){
+    this.applyTheme(theme);
+    localStorage.setItem('themeColor',theme);
+  }
+
+  private applyTheme(theme:string){
+    const body = document.getElementsByTagName('body')[0];
+    body.classList.remove('primary-theme','accent-theme','warn-teheme');
+    body.classList.add(`${theme}-theme`);
+  }
+
+  getTheme():ThemePalette{
+    if(localStorage.getItem('themeColor') === null || localStorage.getItem('themeColor') === undefined){
+      return 'primary'
+    }
+    else{
+      const storedThemeColor = localStorage.getItem('themeColor')
+      return storedThemeColor as ThemePalette;
+    }
+  }
 }
